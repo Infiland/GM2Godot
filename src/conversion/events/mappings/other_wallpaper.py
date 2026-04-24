3edff8e95e660b26587455cb3c7c90840c7e19e1
Infiland <ljubica.citydesign@gmail.com>
2026-04-24 20:12:49 +0200

    Support wallpaper event transpiler

---
+++ src/conversion/events/mappings/other_wallpaper.py
@@ -0,0 +1,7 @@
+from src.conversion.events.base import EventMapping
+
+
+STATIC_MAPPINGS = {
+    (7, 79): EventMapping("_on_wallpaper_config", "", 14, "Other_79.gml"),
+    (7, 81): EventMapping("_on_wallpaper_subscription_data", "", 14, "Other_81.gml"),
+}

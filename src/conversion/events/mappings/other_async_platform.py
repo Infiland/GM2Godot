9990301bcb262d6b063c0d0f957d0092423c61a9
Infiland <ljubica.citydesign@gmail.com>
2026-04-24 14:23:34 +0200

    Support async platform events

---
+++ src/conversion/events/mappings/other_async_platform.py
@@ -0,0 +1,9 @@
+from src.conversion.events.base import EventMapping
+
+
+STATIC_MAPPINGS = {
+    (7, 66): EventMapping("_on_async_in_app_purchase", "", 14, "Other_66.gml"),
+    (7, 67): EventMapping("_on_async_cloud_save", "", 14, "Other_67.gml"),
+    (7, 70): EventMapping("_on_async_social", "", 14, "Other_70.gml"),
+    (7, 71): EventMapping("_on_async_push_notification", "", 14, "Other_71.gml"),
+}

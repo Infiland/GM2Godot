f6201ee5b501ff333abf5275743ae8920e592a5a
Infiland <ljubica.citydesign@gmail.com>
2026-04-24 14:29:55 +0200

    Support room boundary events

---
+++ src/conversion/events/mappings/other_room_boundary.py
@@ -0,0 +1,27 @@
+from src.conversion.events.base import EventMapping
+
+
+STATIC_MAPPINGS = {
+    (7, 0): EventMapping("_on_outside_room", "", 14, "Other_0.gml"),
+    (7, 1): EventMapping("_on_intersect_boundary", "", 14, "Other_1.gml"),
+}
+
+STATIC_MAPPINGS.update({
+    (7, 40 + view_index): EventMapping(
+        f"_on_outside_view_{view_index}",
+        "",
+        14,
+        f"Other_{40 + view_index}.gml",
+    )
+    for view_index in range(8)
+})
+
+STATIC_MAPPINGS.update({
+    (7, 50 + view_index): EventMapping(
+        f"_on_intersect_view_{view_index}_boundary",
+        "",
+        14,
+        f"Other_{50 + view_index}.gml",
+    )
+    for view_index in range(8)
+})
